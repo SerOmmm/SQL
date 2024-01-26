d5f0e67719c1ef37ab7216d1ad1cb11d65ec7d70
Sergei Omelchenko <125047478+SerOmmm@users.noreply.github.com>
2024-01-26 17:09:36 +0300

    Delete Interactive_SQL_trainer_stepik_course/nested_query_step_5.sql

--- Interactive_SQL_trainer_stepik_course/nested_query_step_5.sql
+++
@@ -1,9 +0,0 @@
-/* Вывести информацию о книгах (автор, название, цена), цена которых меньше самой большой из минимальных цен, вычисленных для каждого автора. */
-
-SELECT author, title, price
-FROM book
-WHERE price < ANY (
-    SELECT MIN(price)
-    FROM book
-    GROUP BY author
-    );
