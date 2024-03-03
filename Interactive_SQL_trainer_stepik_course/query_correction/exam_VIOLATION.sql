32f9aad12f2f265478b7c71865a61e2e720be970
Ser Om <ser.ommm@gmail.com>
2024-03-03 23:35:14 +0300

    add exam

--- Interactive_SQL_trainer_stepik_course/query_correction/exam_VIOLATION.sql
+++ Interactive_SQL_trainer_stepik_course/query_correction/exam_VIOLATION.sql
@@ -47,3 +47,24 @@ WHERE
     f.date_payment IS Null AND 
     (f.name, f.number_plate, f.violation) = 
     (query_in.name, query_in.number_plate, query_in.violation);
+
+/* Необходимо:
+- в таблицу fine занести дату оплаты соответствующего штрафа из таблицы payment; 
+- уменьшить начисленный штраф в таблице fine в два раза  (только для тех штрафов, 
+информация о которых занесена в таблицу payment) , если оплата произведена не позднее 20 дней со дня нарушения.*/
+UPDATE fine AS f, payment AS p
+SET 
+    f.sum_fine = IF(DATEDIFF(p.date_payment, p.date_violation) <= 20, f.sum_fine/2, f.sum_fine),
+    f.date_payment = p.date_payment
+WHERE (f.name, f.number_plate, f.date_violation) = (p.name, p.number_plate, p.date_violation);
+
+/* Создать новую таблицу back_payment через запрос, куда внести информацию о неоплаченных штрафах 
+(Фамилию и инициалы водителя, номер машины, нарушение, сумму штрафа  и  дату нарушения) из таблицы fine.*/
+CREATE TABLE back_payment AS
+    SELECT name, number_plate, violation, sum_fine, date_violation
+    FROM fine
+    WHERE date_payment IS NULL;
+
+/* Удалить из таблицы fine информацию о нарушениях, совершенных раньше 1 февраля 2020 года.  */
+DELETE FROM fine
+WHERE date_violation < "2020-02-01";
